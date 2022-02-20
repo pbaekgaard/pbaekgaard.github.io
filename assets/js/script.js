13e73e8017b3037d9c50f378e9f4cbb35bda88a4
flcky@RIG <peterbaekgaard.studie@gmail.com>
2022-02-20 01:07:36 +0100

    added Mobile Layout (not done)

---
+++ assets/js/script.js
@@ -0,0 +1,82 @@
+window.onload = function () {
+const menuBtn = document.querySelector('#hamburger-btn');
+const overlay = document.querySelector('.mobileNav')
+const projects = document.querySelector('#projectsMobile')
+const skills = document.querySelector('#skillsMobile')
+const about = document.querySelector('#aboutMobile')
+const contact = document.querySelector('#contactMobile')
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    overlay.style.width = "100%";
+    menuOpen = true;
+  } else {
+    overlay.style.width = "0px";
+    menuOpen = false;
+  }
+});
+
+projects.addEventListener("click", async function(){
+  if(!menuOpen) {
+    overlay.style.width = "100%";
+    menuOpen = true;
+  } else {
+    overlay.style.width = "0px";
+    menuOpen = false;
+  }
+  stop = true;
+  await sleep(800);
+  stop = false;
+  await type(projectsText);
+});
+
+about.addEventListener("click", async function(){
+  if(!menuOpen) {
+    overlay.style.width = "100%";
+    menuOpen = true;
+  } else {
+    overlay.style.width = "0px";
+    menuOpen = false;
+  }
+  stop = true;
+  await sleep(800);
+  stop = false;
+  await type(aboutText);
+});
+
+skills.addEventListener("click", async function(){
+  if(!menuOpen) {
+    overlay.style.width = "100%";
+    menuOpen = true;
+  } else {
+    overlay.style.width = "0px";
+    menuOpen = false;
+  }
+  stop = true;
+  await sleep(800);
+  stop = false;
+  await type(skillsText);
+});
+
+contact.addEventListener("click", async function(){
+  if(!menuOpen) {
+    overlay.style.width = "100%";
+    menuOpen = true;
+  } else {
+    overlay.style.width = "0px";
+    menuOpen = false;
+  }
+  stop = true;
+  await sleep(800);
+  stop = false;
+  await type(contactText);
+});
+
+
+
+
+
+}
+
