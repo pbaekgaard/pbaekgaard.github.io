1997a249d35f77ac2a093594f7508249ab4590aa
Peter Bækgaard <pbakga21@student.aau.dk>
2023-09-16 16:33:16 +0200

    updated startpage to have searchbar with autocomplete

--- startpage/script.js
+++ startpage/script.js
@@ -1,9 +1,8 @@
-const curEngine = "google";
-const searchEngines = ['google', 'duckduckgo', 'bing', 'librex'];
-const searchBar = document.getElementById('web-search');
-searchBar.addEventListener('keypress', function(e) {
-  if (e.key === 'Enter')
-    search(searchBar.value);
+const curEngine = "duckduckgo";
+const searchEngines = ["google", "duckduckgo", "bing", "librex"];
+const searchBar = document.getElementById("web-search");
+searchBar.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") search(searchBar.value);
 });
 
 function Engine(searchTerm, engine) {
@@ -16,7 +15,6 @@ function Engine(searchTerm, engine) {
       return "https://www.bing.com/search?q=" + searchTerm;
     case "librex":
       return "https://librex.extravi.dev/search.php?q=" + searchTerm;
-
   }
 }
 
@@ -24,3 +22,37 @@ function search(searchTerm) {
   console.log(Engine(searchTerm, curEngine));
   window.location.href = Engine(searchTerm, curEngine);
 }
+
+$("#web-search").autocomplete({
+  delay: 500,
+  minLength: 1,
+  position: { my: "left top-3" },
+  source: function (request, response) {
+    var proxy = "https://api.allorigins.win/get?url=";
+    var suggestURL = "https://duckduckgo.com/ac/?q=%QUERY%";
+    suggestURL = suggestURL.replace("%QUERY%", request.term);
+    suggestURL = suggestURL.replace(/ /g, "+");
+    var requestURL = proxy + encodeURIComponent(suggestURL);
+    $.ajax({
+      type: "GET",
+      url: requestURL,
+      dataType: "jsonp",
+      contentType: "application/json",
+      success: function (data) {
+        var suggestions = [];
+        var res = JSON.parse(data.contents);
+        jQuery.each(res, function (i, val) {
+          suggestions.push(val.phrase);
+        });
+        response(suggestions);
+      },
+    });
+    // fetch(requestURL).then((res) => {
+    // //   if (!res.ok) {
+    // //     return;
+    // //   }
+    // //   var data = res.json();
+    // //   console.log(data);
+    // // });
+  },
+});
